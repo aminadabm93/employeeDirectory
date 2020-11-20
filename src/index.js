2d1f1d4ff90285b2d60a7c5c75529868970296c8
aminadab.morales <aminadabm93@gmail.com>
2020-11-19 21:39:49 -0600

    fixing file structure

---
+++ src/index.js
@@ -0,0 +1,43 @@
+
+import React, { useState } from 'react';
+import { render } from 'react-dom';
+import { AgGridColumn, AgGridReact } from 'ag-grid-react';
+
+import 'ag-grid-enterprise';
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from "./components/navbar";
+
+
+const App = () => {
+    const [gridApi, setGridApi] = useState(null);
+    const [gridColumnApi, setGridColumnApi] = useState(null);
+
+    const [rowData, setRowData] = useState([
+        {first: "Solana", last: "Rowe", title: "apprentice"},
+        {first: "Britney", last: "Spears", title: "manager"},
+        {first: "Beyonce", last: "Knowles", title: "performer"}
+    ]);
+
+    function onGridReady(params) {
+        setGridApi(params.api);
+        setGridColumnApi(params.columnApi);
+    }
+
+    return (
+      
+        <div className="ag-theme-alpine" style={ { height: 400, width: 600 } }>
+          <Navbar />
+            <AgGridReact
+                onGridReady={onGridReady}
+                rowData={rowData}>
+                <AgGridColumn field="first" sortable={true} filter={true}></AgGridColumn>
+                <AgGridColumn field="last" sortable={true} filter={true}></AgGridColumn>
+                <AgGridColumn field="title" sortable={true} filter={true}></AgGridColumn>
+            </AgGridReact>
+        </div>
+    );
+};
+
+render(<App />, document.getElementById('root'));
